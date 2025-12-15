47ea6af1ae88723d448069aaf80a33a819a546ef
eyeblech <0x1123@proton.me>
2025-12-15 20:37:03 +0545

    recycled

--- donor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Donor
-
-@admin.register(Donor)
-class DonorAdmin(admin.ModelAdmin):
-    list_display = ['user', 'blood_group', 'phone', 'last_donation_date']
